7b5589c02b092f2f1df72278ca76909259804586
cinquemb <1519017+cinquemb@users.noreply.github.com>
2023-09-08 08:58:34 -0400

    svelteit updates, meant to run locally with https dev server

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,34 @@ const config = {
 	preprocess: preprocess(),
 	kit: {
 		adapter: adapter(),
-		methodOverride: {
+		/*methodOverride: {
 			allowed: ['PATCH', 'DELETE']
-		}
+		}*/
 	}
 };
 
 export default config;
+
+
+/*
+
+
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
+export default {
+	preprocess: preprocess(),
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    }
+};
+
+*/
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert'
+
+export default defineConfig({
+	plugins: [sveltekit(), mkcert()],
+ 	server: {
+		cors: true,    
+        https: true,
+        proxy: {}
+    },
+});
